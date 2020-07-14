086042a0858c1d61c645a845f6463b121dfa97fd
Sebastián Salazar <sebanicosp@gmail.com>
2020-07-14 00:02:54 -0400

    add fourth page

---
+++ lib/src/ui/fourthPage/fourth_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:personal_app/src/ui/commons/common_body.dart';
+
+class FourthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CommonBody(child: Center());
+  }
+}
